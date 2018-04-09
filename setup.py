a15cbb095ad4856bd49fbf46a68e0c623f41ae39
Phil Austin <paustin@eos.ubc.ca>
2018-04-09 11:15:15 -0700

    make VERSION sole source

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ import e340py
 
 from setuptools import setup, find_packages
 
-# version_file = open(os.path.join('e340py', 'VERSION'))
-# version = version_file.read().strip()
+version_file = open(os.path.join('e340py', 'VERSION'))
+version = version_file.read().strip()
 
 setup(
     name = "e340py",
     packages=find_packages(),
     #version=e340py.__version__,
-    version="0.1.0",
+    #version="0.1.0",
+    version=version,
     entry_points={
           'console_scripts': [
               'dump_comments = e340py.dump_comments:main',
