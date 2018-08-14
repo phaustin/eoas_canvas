93139849d7113542e26e9fd644569bc15ccff758
Phil Austin <paustin@eos.ubc.ca>
2018-08-14 07:57:49 -0700

    turn on debugging

--- scripts/change_module.py
+++ scripts/change_module.py
@@ -4,6 +4,23 @@ import json
 from pathlib import Path
 import os
 
+
+import requests
+import logging
+
+# Enabling debugging at http.client level (requests->urllib3->http.client)
+# you will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# the only thing missing will be the response.body which is not logged.
+from http.client import HTTPConnection
+HTTPConnection.debuglevel = 1
+
+logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from requests
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+
 my_home=Path(os.environ['HOME'])
 full_path=my_home / Path('.canvas.json')
 with open(full_path,'r') as f:
